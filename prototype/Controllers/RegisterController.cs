32439b4a624282c7f715a04fafcc343a72213738
xynoan <ncmorva@gmail.com>
2024-10-22 16:51:15 +0800

    work on register pages

---
+++ prototype/Controllers/RegisterController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prototype.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Education()
+        {
+            return View();
+        }
+
+        public IActionResult Emergency()
+        {
+            return View();
+        }
+
+        public IActionResult Family()
+        {
+            return View();
+        }
+
+        public IActionResult PersonalInfo()
+        {
+            return View();
+        }
+
+        public IActionResult Registered()
+        {
+            return View();
+        }
+
+        public IActionResult Verification()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
