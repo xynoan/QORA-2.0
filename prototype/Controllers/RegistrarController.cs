de2a0470758a6586b5dd2f61c4b7d7ef7e5af1d9
xynoan <ncmorva@gmail.com>
2024-10-25 13:34:25 +0800

    finish conversion

---
+++ prototype/Controllers/RegistrarController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prototype.Controllers
+{
+    public class RegistrarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddCourse()
+        {
+            return View();
+        }
+        public IActionResult Enlist()
+        {
+            return View();
+        }
+        public IActionResult Printing()
+        {
+            return View();
+        }
+        public IActionResult UpdateCourse()
+        {
+            return View();
+        }
+        public IActionResult ViewProfile()
+        {
+            return View();
+        }
+    }
+}
